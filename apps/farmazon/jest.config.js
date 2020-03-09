3a42d68cdd315a27326ea8f3dbd0b5ed127de999
Younes Jaaidi <yjaaidi@gmail.com>
2020-03-09 17:59:25 +0100

    initial commit

---
+++ apps/farmazon/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'farmazon',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/farmazon',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js'
+  ]
+};
