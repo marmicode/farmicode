565d2d3b4f2536d7deb2d2caf0e6be1416a522b9
Younes Jaaidi <yjaaidi@gmail.com>
2020-03-13 09:21:33 +0100

    🚧 search animals: add harness for <fz-animal-list>

---
+++ apps/farmazon/src/app/animal-search/animal-list/animal-list.component.harness.ts
@@ -0,0 +1,12 @@
+import { ComponentHarness } from '@angular/cdk/testing';
+
+export class AnimalListHarness extends ComponentHarness {
+  static hostSelector = 'fz-animal-list';
+
+  private _getAnimalNameElList = this.locatorForAll('li');
+
+  async getAnimalNameList(): Promise<string[]> {
+    const elList = await this._getAnimalNameElList();
+    return Promise.all(elList.map(el => el.text()));
+  }
+}
