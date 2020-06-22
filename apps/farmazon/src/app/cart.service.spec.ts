86f49dee4d05708c771cb0c7f1cb9592321200a0
Younes Jaaidi <yjaaidi@gmail.com>
2020-06-22 16:21:24 +0200

    🛠 use new Animal type

--- apps/farmazon/src/app/cart.service.spec.ts
+++ apps/farmazon/src/app/cart.service.spec.ts
@@ -19,7 +19,7 @@ describe('CartService', () => {
     const dolly = createAnimal({
       id: 'dolly',
       type: AnimalType.Sheep,
-      gender: Gender.Other,
+      gender: Gender.Female,
       price: 1000
     });
     const missy = createAnimal({
