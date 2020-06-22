f11536f59fb29688d1e61b38c042cfef44ae360c
Younes Jaaidi <yjaaidi@gmail.com>
2020-06-22 16:01:46 +0200

    ✅ compute animal price

---
+++ apps/farmazon/src/app/compute-animal-price.spec.ts
@@ -0,0 +1,71 @@
+import { AnimalType, createAnimal, Gender } from './animal';
+import { computeAnimalPrice } from './compute-animal-price';
+
+describe('computeAnimalPrice', () => {
+  let now;
+
+  beforeEach(() => {
+    /* @todo replace this by mocks when we'll learn more about them. */
+    now = Date.now;
+    Date.now = () => new Date(2025, 5, 1).getTime();
+  });
+
+  afterEach(() => {
+    Date.now = now;
+  });
+
+  it('should offer free cats', () => {
+    const cat = createAnimal({
+      type: AnimalType.Cat,
+      gender: Gender.Female,
+      birthDate: new Date(2025, 1, 1)
+    });
+    expect(computeAnimalPrice(cat)).toEqual(0);
+  });
+
+  it('should compute dog price', () => {
+    const dog = createAnimal({
+      type: AnimalType.Dog,
+      gender: Gender.Male,
+      birthDate: new Date(2025, 1, 1)
+    });
+    expect(computeAnimalPrice(dog)).toEqual(300);
+  });
+
+  it('should compute sheep price', () => {
+    const sheep = createAnimal({
+      type: AnimalType.Sheep,
+      gender: Gender.Male,
+      birthDate: new Date(2025, 1, 1)
+    });
+    expect(computeAnimalPrice(sheep)).toEqual(100);
+  });
+
+  it('should compute double female price', () => {
+    const sheep = createAnimal({
+      type: AnimalType.Sheep,
+      gender: Gender.Female,
+      birthDate: new Date(2025, 1, 1)
+    });
+    expect(computeAnimalPrice(sheep)).toEqual(200);
+  });
+
+  it('should decrease price depending on age', () => {
+    const oldDog = createAnimal({
+      type: AnimalType.Dog,
+      gender: Gender.Male,
+      /* 4 years old. */
+      birthDate: new Date(2020, 12, 1)
+    });
+    expect(computeAnimalPrice(oldDog)).toEqual(60);
+  });
+
+  it('should offer old animals', () => {
+    const veryOldDog = createAnimal({
+      type: AnimalType.Dog,
+      gender: Gender.Male,
+      birthDate: new Date(2000, 1, 1)
+    });
+    expect(computeAnimalPrice(veryOldDog)).toEqual(0);
+  });
+});
---
+++ apps/farmazon/src/app/compute-animal-price.ts
@@ -0,0 +1,20 @@
+import { Animal, AnimalType, Gender } from './animal';
+
+export const priceMap = new Map([
+  [AnimalType.Cat, 0],
+  [AnimalType.Dog, 300],
+  [AnimalType.Sheep, 100]
+]);
+
+export const millisecondsPerYear = 24 * 3600 * 365 * 1000;
+
+export function computeAnimalPrice(animal: Animal): number {
+  const price = priceMap.get(animal.type);
+  const initialPrice = animal.gender === Gender.Female ? price * 2 : price;
+
+  const age = Math.floor(
+    (Date.now() - animal.birthDate.getTime()) / millisecondsPerYear
+  );
+
+  return Math.round(initialPrice * Math.max(0, 1 - 0.2 * age));
+}
